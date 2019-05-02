37a9738d119e420b3bb5fba552f94f6d7f7f4a1a
thedewi <thedewi@users.noreply.github.com>
2019-05-02 23:26:21 +1000

    add AdoUrl tests

---
+++ source/Server.Tests/AdoUrlScenarios.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Octopus.Server.Extensibility.IssueTracker.AzureDevOps.AdoClients;
+
+namespace Server.Tests
+{
+    [TestFixture]
+    public class AdoUrlScenarios
+    {
+        [TestCase("http://redstoneblock/DefaultCollection/Deployable/_build/results?buildId=24",
+            "http://redstoneblock/DefaultCollection",
+            "http://redstoneblock/DefaultCollection/Deployable",
+            24)]
+        [TestCase("https://dev.azure.com/Barsonax/Singularity//_build//results?view=logs&buildId=160&lineStart=34&taskId=7dae026f-9e99-5d65-075e-3f7579577f94",
+            "https://dev.azure.com/Barsonax",
+            "https://dev.azure.com/Barsonax/Singularity",
+            160)]
+        public void ValidBuildBrowserUrlsAreParsedCorrectly(string browserUrl, string expOrgUrl, string expProjUrl, int expBuildId)
+        {
+            var abu = AdoBuildUrls.ParseBrowserUrl(browserUrl);
+            Assert.AreEqual(expOrgUrl, abu.OrganizationUrl);
+            Assert.AreEqual(expProjUrl, abu.ProjectUrl);
+            Assert.AreEqual(expBuildId, abu.BuildId);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("http://redstoneblock/DefaultCollection/Deployable")]
+        [TestCase("ftp://redstoneblock/DefaultCollection/Deployable/_build/results?buildId=24")]
+        [TestCase("http://redstoneblock/DefaultCollection/Deployable/_build/results?buildId=29af")]
+        public void InvalidBuildBrowserUrlsThrow(string browserUrl)
+        {
+            Assert.Throws<ArgumentException>(() => AdoBuildUrls.ParseBrowserUrl(browserUrl));
+        }
+    }
+}
--- source/Server/AdoClients/AdoUrl.cs
+++ source/Server/AdoClients/AdoUrl.cs
@@ -18,14 +18,14 @@ namespace Octopus.Server.Extensibility.IssueTracker.AzureDevOps.AdoClients
     {
         public int BuildId { get; set; }
 
-        public static AdoBuildUrls ParseBrowserUrl(string browseUrl)
+        public static AdoBuildUrls ParseBrowserUrl(string browserUrl)
         {
             ArgumentException ParseError(Exception innerException = null)
-                => new ArgumentException("Unrecognized build browse URL.", nameof(browseUrl), innerException);
+                => new ArgumentException("Unrecognized build browse URL.", nameof(browserUrl), innerException);
 
             try
             {
-                var prefixMatch = Regex.Match(browseUrl, @"^((.+)/[^\/]+)/_build");
+                var prefixMatch = Regex.Match(browserUrl, @"^\s*((https?://.+?)/+[^\/]+)/+_build\b");
                 if (!prefixMatch.Success)
                 {
                     throw ParseError();
@@ -35,7 +35,7 @@ namespace Octopus.Server.Extensibility.IssueTracker.AzureDevOps.AdoClients
                 {
                     OrganizationUrl = prefixMatch.Groups[2].Value,
                     ProjectUrl = prefixMatch.Groups[1].Value,
-                    BuildId = int.Parse(new Uri(browseUrl, UriKind.Absolute).ParseQueryString()["buildId"])
+                    BuildId = int.Parse(new Uri(browserUrl, UriKind.Absolute).ParseQueryString()["buildId"])
                 };
             }
             catch (Exception ex)
