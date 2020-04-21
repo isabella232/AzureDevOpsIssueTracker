0a4b1025b1023014e501f1f16901e75abb7909f8
Shannon Lewis <slewis74@gmail.com>
2020-04-21 10:43:13 +1000

    updates for Nevermore2

--- source/Server/Configuration/AzureDevOpsConfiguration.cs
+++ source/Server/Configuration/AzureDevOpsConfiguration.cs
@@ -5,9 +5,8 @@ namespace Octopus.Server.Extensibility.IssueTracker.AzureDevOps.Configuration
 {
     class AzureDevOpsConfiguration : ExtensionConfigurationDocument
     {
-        public AzureDevOpsConfiguration() : base("AzureDevOps", "Octopus Deploy", "1.0")
+        public AzureDevOpsConfiguration() : base(AzureDevOpsConfigurationStore.SingletonId, "AzureDevOps", "Octopus Deploy", "1.0")
         {
-            Id = AzureDevOpsConfigurationStore.SingletonId;
         }
 
         public string BaseUrl { get; set; }
